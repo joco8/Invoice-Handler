9829e7522d750e78b9a694e6c0602cdce744d606
Josh Cohen <janc11832@gmail.com>
2020-08-13 14:44:27 -0400

    Removed everthing

--- variables.py
+++
@@ -1,8 +0,0 @@
-#folders either need to be in the same directory as this program or be full direct paths 
-folder_with_pdfs="pdfs-folder" 
-folder_vendor_usage="vendor_usage"
-folder_for_renamed_pdfs="renamedPdfs"
-folder_for_csv="inv_extracted_data"
-AP_csv_file_name="desktop-ocr-updated.csv"
-vendor_usage_running_tab="vendor_running_tab.csv"
-
